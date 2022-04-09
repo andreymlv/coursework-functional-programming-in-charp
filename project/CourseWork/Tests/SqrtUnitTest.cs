b1e9f97329ef9e0cdcf94b8fc28b75b5c0497eb5
andreymlv <ndrmlv@ya.ru>
2022-04-09 09:31:26 +0300

    Добавлены примитивные тесты

---
+++ project/CourseWork/Tests/SqrtUnitTest.cs
@@ -0,0 +1,29 @@
+﻿using Xunit;
+using Library;
+using System;
+
+namespace Tests;
+
+public class SqrtUnitTest
+{
+    [Theory]
+    [InlineData(1, 1)]
+    [InlineData(4, 2)]
+    [InlineData(8, 2.828427)]
+    public void RecursiveInvokeTest(double x, double expected)
+    {
+        var actual = Sqrt.RecursiveInvoke(x);
+        Assert.True(Math.Abs(expected - actual) < Sqrt.Epsilon);
+    }
+
+    [Theory]
+    [InlineData(1, 1)]
+    [InlineData(4, 2)]
+    [InlineData(8, 2.828427)]
+    public void TailRecursiveInvokeTest(double x, double expected)
+    {
+        var actual = Sqrt.TailRecursiveInvoke(x);
+        Assert.True(Math.Abs(expected - actual) < Sqrt.Epsilon);
+    }
+}
+
--- project/CourseWork/Tests/SumUnitTest.cs
+++ project/CourseWork/Tests/SumUnitTest.cs
@@ -1,11 +1,50 @@
 using Xunit;
+using Library;
 
 namespace Tests;
 
 public class SumUnitTest
 {
-    [Fact]
-    public void RecursiveTest()
+    private static double Term(double x) => x;
+    private static double Next(double x) => x + 1;
+
+    [Theory]
+    [InlineData(1, 1, 1)]
+    [InlineData(1, 2, 3)]
+    [InlineData(-5, 5, 0)]
+    public void RecursiveInvokeTest(double from, double to, double expected)
     {
+        var actual = Sum.RecursiveInvoke(Term, from, Next, to);
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData(1, 1, 1)]
+    [InlineData(1, 2, 3)]
+    [InlineData(-5, 5, 0)]
+    public void TailRecursiveInvokeTest(double from, double to, double expected)
+    {
+        var actual = Sum.TailRecursiveInvoke(Term, from, Next, to);
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData(1, 1, 1)]
+    [InlineData(1, 2, 3)]
+    [InlineData(-5, 5, 0)]
+    public void ImperativeInvokeTest(double from, double to, double expected)
+    {
+        var actual = Sum.ImperativeInvoke(Term, from, Next, to);
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData(1, 1, 1)]
+    [InlineData(1, 2, 3)]
+    [InlineData(-5, 5, 0)]
+    public void DotNetInvokeTest(int from, int to, double expected)
+    {
+        var actual = Sum.DotNetInvoke(Term, from, Next, to);
+        Assert.Equal(expected, actual);
     }
 }
